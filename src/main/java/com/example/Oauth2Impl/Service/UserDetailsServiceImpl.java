f395907d8592992dc351b5e44e17ee510e919fdf
Sachithra Dilshan <itismesachithra@gmail.com>
2020-09-23 07:14:27 +0530

    first commit

---
+++ src/main/java/com/example/Oauth2Impl/Service/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.Oauth2Impl.Service;
+
+import com.example.Oauth2Impl.Model.AuthUserDetail;
+import com.example.Oauth2Impl.Model.User;
+import com.example.Oauth2Impl.Repositery.UserDetailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service("userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserDetailRepository userDetailRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userDetailRepository.findByUsername(name);
+
+        optionalUser.orElseThrow(() -> new UsernameNotFoundException("Username or password wrong"));
+
+        UserDetails userDetails = new AuthUserDetail(optionalUser.get());
+        new AccountStatusUserDetailsChecker().check(userDetails);
+        return userDetails;
+    }
+}
