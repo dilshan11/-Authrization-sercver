d7325028bef10af9285daa95faa015a36f587fd0
Sachithra Dilshan <itismesachithra@gmail.com>
2020-09-24 07:44:49 +0530

    Getting currently log in user details

---
+++ src/main/java/com/example/Oauth2Impl/Controller/AuthController.java
@@ -0,0 +1,15 @@
+//package com.example.Oauth2Impl.Controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.security.Principal;
+//
+//@RestController
+//public class AuthController {
+//
+//    @GetMapping("/userinfo")
+//    public Principal me(Principal principal) {
+//        return principal;
+//    }
+//}
